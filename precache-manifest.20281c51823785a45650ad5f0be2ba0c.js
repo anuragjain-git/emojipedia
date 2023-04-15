8341f74a33c05990a0ebb69ec33348fd9e5df066
Anurag Jain <itsanuragjain.03@gmail.com>
2023-04-15 22:45:35 +0530

    Updates

---
+++ precache-manifest.20281c51823785a45650ad5f0be2ba0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1524b6e036beaa422c44b71c22231f",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "7e441519b3f912100b2b",
+    "url": "/emojipedia/static/js/2.086d1643.chunk.js"
+  },
+  {
+    "revision": "8a5874f76cb702844f30",
+    "url": "/emojipedia/static/js/main.07164f78.chunk.js"
+  },
+  {
+    "revision": "b20d33b5dc7f0ae7b069",
+    "url": "/emojipedia/static/js/runtime-main.90a04422.js"
+  }
+]);
